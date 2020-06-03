060884d8270ee39364636c52ee8a466829220e85
Astarosa <baptiste.shortgibet@gmail.com>
2020-06-03 15:52:54 +0200

    #3 Menu wip

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,11 +1,8 @@
 import React, { useState } from 'react';
 import { Layout, Menu } from 'antd';
 import {
-  DesktopOutlined,
-  PieChartOutlined,
-  FileOutlined,
-  TeamOutlined,
-  UserOutlined
+  FormOutlined,
+  TeamOutlined
 } from '@ant-design/icons';
 
 const { Sider } = Layout;
@@ -13,35 +10,28 @@ const { SubMenu } = Menu;
 
 const SideBar = () => {
   const [collapsed, setCollapsed] = useState(false);
-  const onCollapse = (collapsed) => {
-    console.log(collapsed);
-    setCollapsed({ collapsed });
+  const onCollapse = () => {
+    setCollapsed(!collapsed);
   };
   return (
     <Sider
-      onClick={() => setCollapsed(!collapsed)}
       collapsible
       collapsed={collapsed}
-      onCollapse={onCollapse}
+      onCollapse={() => { onCollapse(); }}
     >
       <div className='logo' />
       <Menu theme='dark' defaultSelectedKeys={['1']} mode='inline'>
-        <Menu.Item key='1' icon={<PieChartOutlined />}>
-          Option 1
-        </Menu.Item>
-        <Menu.Item key='2' icon={<DesktopOutlined />}>
-          Option 2
-        </Menu.Item>
-        <SubMenu key='sub1' icon={<UserOutlined />} title='User'>
-          <Menu.Item key='3'>Tom</Menu.Item>
-          <Menu.Item key='4'>Bill</Menu.Item>
-          <Menu.Item key='5'>Alex</Menu.Item>
+        <SubMenu key='sub1' icon={<FormOutlined />} title='Articles'>
+          <Menu.Item key='1'>Tous les Articles</Menu.Item>
+          <Menu.Item key='2'>Ajouter</Menu.Item>
         </SubMenu>
-        <SubMenu key='sub2' icon={<TeamOutlined />} title='Team'>
-          <Menu.Item key='6'>Team 1</Menu.Item>
-          <Menu.Item key='8'>Team 2</Menu.Item>
+        <SubMenu key='sub2' icon={<FormOutlined />} title='Recettes'>
+          <Menu.Item key='3'>Toutes les recettes</Menu.Item>
+          <Menu.Item key='4 '>Ajouter</Menu.Item>
         </SubMenu>
-        <Menu.Item key='9' icon={<FileOutlined />} />
+        <SubMenu key='sub2' icon={<TeamOutlined />} title='Utilisateurs'>
+          <Menu.Item key='5'>Gérer les utilisateurs</Menu.Item>
+        </SubMenu>
       </Menu>
     </Sider>
   );
