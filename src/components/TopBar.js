1d0c245cb54e5bd7fa9fabb4f0bf856907536716
Anaïs Huoch <anaisgagneur@gmail.com>
2020-06-03 14:31:10 +0200

    breadcrumb fixing

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Layout } from 'antd';
+import { Breadcrumb } from 'antd';
 
 const { Header } = Layout;
 
@@ -7,6 +8,11 @@ const TopBar = () => {
   return (
     <>
       <Header className='site-layout-background' style={{ padding: 0 }} />
+      <Breadcrumb style={{ margin: '16px 0' }}>
+      <Breadcrumb.Item>User</Breadcrumb.Item>
+      <Breadcrumb.Item>Bill</Breadcrumb.Item>
+      </Breadcrumb>
+
     </>
   );
 };
