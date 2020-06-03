0d9ac1f091d8c50af32b9aea844a97c5bda16805
tmicka23 <mickathp42@gmail.com>
2020-06-03 11:59:52 +0200

    #3 wip layout cutting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import Menu from './components/Menu';
-import './Styles/App.css';
+import React from "react";
+import { Layout } from "antd";
+import SideBar from "./components/SideBar";
+import TopBar from "./components/TopBar";
+import "./Styles/App.css";
+const { Content } = Layout;
 
-function App () {
+function App() {
   return (
     <div className='App'>
-      <Menu />
+      <Layout style={{ minHeight: "100vh" }}>
+        <SideBar />
+        <Layout className='site-layout'>
+          <TopBar />
+          <Content style={{ margin: "0 16px" }}>
+            <div
+              className='site-layout-background'
+              style={{ padding: 24, minHeight: 360 }}>
+              Bill is a cat.
+            </div>
+          </Content>
+        </Layout>
+      </Layout>
     </div>
   );
 }
---
+++ src/components/TopBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Layout } from "antd";
+
+const { Header, Breadcrumb } = Layout;
+
+const TopBar = () => {
+  return (
+    <>
+      <Header className='site-layout-background' style={{ padding: 0 }} />
+    </>
+  );
+};
+
+export default TopBar;
