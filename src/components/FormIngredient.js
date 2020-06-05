50849ef92adaf11b03177aefbbd18899d6b51b63
Astarosa <baptiste.shortgibet@gmail.com>
2020-06-05 12:54:44 +0200

    #4 fix some shitty css

--- src/components/FormIngredient.js
+++ src/components/FormIngredient.js
@@ -1,24 +1,60 @@
 import React from 'react';
 import 'antd/dist/antd.css';
-import { Form, Input, Checkbox } from 'antd';
-
-const FormItem = Form.Item;
+import { Form, Input, Space, Button, Checkbox } from 'antd';
+import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons'
 
 const FormIngredient = () => {
+  const onFinish = values => {
+    console.log('Received values of form:', values);
+  };
+
   return (
-    <div>
-      <Form className='form-ingredient' style={{ width: '250px', margin: 'auto' }}>
-        <FormItem label='Ingredient'>
-          <Input
-            type='text'
-            placeholder="Nom de l'ingrédient"
-          />
-        </FormItem>
-        <FormItem>
-          <Checkbox>Potentiellement allergène</Checkbox>
-        </FormItem>
-      </Form>
-    </div>
+    <Form name="dynamic_form_nest_item" onFinish={onFinish} autoComplete="off">
+        <Form.List name="ingredients">
+        {(fields, { add, remove }) => {
+          return (
+            <div>
+              {fields.map(field => (
+                <Space key={field.key} style={{ display: 'flex', marginBottom: 8 }} align="start">
+                  <Form.Item
+                    {...field}
+                    name={[field.name, 'ingredient']}
+                    fieldKey={[field.fieldKey, 'ingredient-name']}
+                    rules={[{ required: true, message: 'Missing ingredient name' }]}
+                  >
+                  <Input placeholder="First Name" />
+                  </Form.Item>
+                  <Form.Item
+                    {...field}
+                    fieldKey={[field.fieldKey, 'allergen']}
+                  >
+                    <Checkbox>Allergène</Checkbox>
+                  </Form.Item>
+
+                  <MinusCircleOutlined
+                    onClick={() => {
+                      remove(field.name);
+                    }}
+                  />
+                </Space>
+              ))}
+
+              <Form.Item>
+                <Button
+                  type="dashed"
+                  onClick={() => {
+                    add();
+                  }}
+                  block
+                >
+                  <PlusOutlined /> Add field
+                </Button>
+              </Form.Item>
+            </div>
+          );
+        }}
+      </Form.List>
+    </Form>
   );
 };
 
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import { NavLink, Link } from 'react-router-dom';
-import { Layout, Menu } from 'antd';
+import { Layout, Menu} from 'antd';
 import {
   BookOutlined,
   FormOutlined,
   TeamOutlined,
+  UserOutlined,
   BarChartOutlined,
   CopyOutlined
 } from '@ant-design/icons';
@@ -13,6 +14,7 @@ import logo from '../images/healthymood-logo.png';
 import smallLogo from '../images/healthymood-small.png';
 
 const { Sider } = Layout;
+
 const { SubMenu } = Menu;
 
 const SideBar = () => {
@@ -22,82 +24,87 @@ const SideBar = () => {
   };
 
   return (
-    <Sider
-      collapsible
-      collapsed={collapsed}
-      onCollapse={() => {
-        onCollapse();
-      }}
-    >
-      <div className='logo-container'>
-        <Link exact to='/' className='logo'>
-          <img src={collapsed ? smallLogo : logo} alt='HealthyMood' />
-        </Link>
-      </div>
-      <Menu theme='dark' defaultSelectedKeys={['0']} mode='inline'>
-        <Menu.Item key='0' icon={<BarChartOutlined />}>
-          <NavLink exact to='/'>Dashboard</NavLink>
-        </Menu.Item>
-        <SubMenu key='sub1' icon={<FormOutlined />} title='Articles'>
-          <Menu.Item key='1'>
-            <NavLink exact to='/articles'>Tous les Articles</NavLink>
+    
+      <Sider 
+        collapsible
+        collapsed={collapsed}
+        onCollapse={() => {
+          onCollapse();
+        }}
+      >
+        <div className="logo-container">
+          <Link exact to="/" className="logo">
+            <img src={collapsed ? smallLogo : logo} alt="HealthyMood" />
+          </Link>
+        </div>
+        <Menu theme="dark" defaultSelectedKeys={["0"]} mode="inline">
+          <Menu.Item key="0" icon={<BarChartOutlined />}>
+            <NavLink exact to="/">
+              Dashboard
+            </NavLink>
           </Menu.Item>
-          <Menu.Item key='2'>Ajouter</Menu.Item>
+          <Menu.Item key="1" icon={<FormOutlined />}>
+            <NavLink exact to="/articles">
+              Articles
+            </NavLink>
+          </Menu.Item>
+          <Menu.Item key="3" icon={<FormOutlined />}>
+            <NavLink exact to="/recettes">
+              Recettes
+            </NavLink>
+          </Menu.Item>
+          <SubMenu key='sub1' icon={<BookOutlined />} title='Catégories'>
+            <Menu.Item key="5">
+              <NavLink exact to="/categories-articles">
+                Catégories articles
+              </NavLink>
+            </Menu.Item>
+            <Menu.Item key="6">
+              <NavLink exact to="/categories-recettes">
+                Catégories recettes
+              </NavLink>
+            </Menu.Item>
         </SubMenu>
-        <SubMenu key='sub2' icon={<FormOutlined />} title='Recettes'>
-          <Menu.Item key='3'>
-            <NavLink exact to='/recettes'>Toutes les recettes</NavLink>
+          <Menu.Item key="7" icon={<FormOutlined />}>
+            <NavLink exact to="/ingredients">
+              Ingrédients
+            </NavLink>
           </Menu.Item>
-          <Menu.Item key='4'>Ajouter</Menu.Item>
-        </SubMenu>
-        <SubMenu key='sub3' icon={<BookOutlined />} title='Catégories'>
-          <Menu.Item key='5'>
-            <NavLink exact to='/categories-articles'>Catégories Articles</NavLink>
+          <Menu.Item key="9" icon={<FormOutlined />}>
+            <NavLink exact to="/types-plats">
+              Types de plats
+            </NavLink>
           </Menu.Item>
-          <Menu.Item key='6'>
-            <NavLink exact to='/categories-recettes'>Catégories Recettes</NavLink>
+          <Menu.Item key="11" icon={<FormOutlined />}>
+            <NavLink exact to="/types-repas">
+              Types de repas
+            </NavLink>
           </Menu.Item>
-        </SubMenu>
-        <SubMenu key='sub4' icon={<FormOutlined />} title='Ingrédients'>
-          <Menu.Item key='7'>
-            <NavLink exact to='/ingredients'>Tous les ingrédients</NavLink>
+          <Menu.Item key="13" icon={<FormOutlined />}>
+            <NavLink exact to="/regimes">
+              Tous les Régimes
+            </NavLink>
           </Menu.Item>
-          <Menu.Item key='8'>Ajouter</Menu.Item>
-        </SubMenu>
-        <SubMenu key='sub5' icon={<FormOutlined />} title='Types de Plats'>
-          <Menu.Item key='9'>
-            <NavLink exact to='/types-plats'>Tous les plats</NavLink>
+
+          <Menu.Item key="15" icon={<CopyOutlined />}>
+            <NavLink exact to="/pages">
+              Toutes les pages
+            </NavLink>
           </Menu.Item>
-          <Menu.Item key='10'>Ajouter</Menu.Item>
-        </SubMenu>
-        <SubMenu key='sub6' icon={<FormOutlined />} title='Types de Repas'>
-          <Menu.Item key='11'>
-            <NavLink exact to='/types-repas'>Tous les repas</NavLink>
+
+          <Menu.Item key="17" icon={<TeamOutlined />}>
+            <NavLink exact to="/utilisateurs">
+              Gérer les utilisateurs
+            </NavLink>
           </Menu.Item>
-          <Menu.Item key='12'>Ajouter</Menu.Item>
-        </SubMenu>
-        <SubMenu key='sub7' icon={<FormOutlined />} title='Régimes'>
-          <Menu.Item key='13'>
-            <NavLink exact to='/regimes'>Tous les Régimes</NavLink>
+          <Menu.Item key="18" icon={<UserOutlined />}>
+            <NavLink exact to="/mon-profil">
+              Mon Profil
+            </NavLink>
           </Menu.Item>
-          <Menu.Item key='14'>Ajouter</Menu.Item>
-        </SubMenu>
-        <SubMenu key='sub8' icon={<CopyOutlined />} title='Pages'>
-          <Menu.Item key='15'>
-            <NavLink exact to='/pages'>Toutes les pages</NavLink>
-          </Menu.Item>
-          <Menu.Item key='16'>Ajouter</Menu.Item>
-        </SubMenu>
-        <SubMenu key='sub9' icon={<TeamOutlined />} title='Utilisateurs'>
-          <Menu.Item key='17'>
-            <NavLink exact to='/utilisateurs'>Gérer les utilisateurs</NavLink>
-          </Menu.Item>
-          <Menu.Item key='18'>
-            <NavLink exact to='/mon-profil'>Mon Profil</NavLink>
-          </Menu.Item>
-        </SubMenu>
-      </Menu>
-    </Sider>
+        </Menu>
+      </Sider>
+    
   );
 };
 
