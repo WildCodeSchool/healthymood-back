881e17c3a3beab239ae0cf471a7151ba709116b5
tmicka23 <mickathp42@gmail.com>
2020-06-11 09:50:09 +0200

    clean console warning

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -18,14 +18,18 @@ const { SubMenu } = Menu;
 
 const SideBar = () => {
   const [collapsed, setCollapsed] = useState(false);
-  const onCollapse = () => { setCollapsed(!collapsed); };
+  const onCollapse = () => {
+    setCollapsed(!collapsed);
+  };
 
   return (
     <Sider
       style={{ width: '500px' }}
       collapsible
       collapsed={collapsed}
-      onCollapse={() => { onCollapse(); }}
+      onCollapse={() => {
+        onCollapse();
+      }}
     >
       <div className='logo-container'>
         <Link to='/' className='logo'>
@@ -34,88 +38,61 @@ const SideBar = () => {
       </div>
       <Menu theme='dark' defaultSelectedKeys={['0']} mode='inline'>
         <Menu.Item key='0' icon={<BarChartOutlined />}>
-          <Link to='/'>
-            Dashboard
-          </Link>
+          <Link to='/'>Dashboard</Link>
         </Menu.Item>
         <SubMenu key='sub1' icon={<FormOutlined />} title='Articles'>
           <Menu.Item key='1'>
-            <Link exact to='/articles'>Nos articles</Link>
+            <Link to='/articles'>Nos articles</Link>
           </Menu.Item>
           <Menu.Item key='2'>
-            <Link exact to='/articles/ajouter'>Ajouter</Link>
+            <Link to='/articles/ajouter'>Ajouter</Link>
           </Menu.Item>
         </SubMenu>
         <SubMenu key='sub2' icon={<FormOutlined />} title='Recettes'>
           <Menu.Item key='3'>
-            <Link to='/recettes'>
-              Nos recettes
-            </Link>
+            <Link to='/recettes'>Nos recettes</Link>
           </Menu.Item>
           <Menu.Item key='4'>
-            <Link to='/recettes/ajouter'>
-              Ajouter
-            </Link>
+            <Link to='/recettes/ajouter'>Ajouter</Link>
           </Menu.Item>
         </SubMenu>
 
         <SubMenu key='sub3' icon={<BookOutlined />} title='Catégories'>
           <Menu.Item key='5'>
-            <Link to='/categories-articles'>
-              Catégories articles
-            </Link>
+            <Link to='/categories-articles'>Catégories articles</Link>
           </Menu.Item>
           <Menu.Item key='6'>
-            <Link to='/categories-recettes'>
-              Catégories recettes
-            </Link>
+            <Link to='/categories-recettes'>Catégories recettes</Link>
           </Menu.Item>
         </SubMenu>
         <Menu.Item key='7' icon={<FormOutlined />}>
-          <Link to='/ingredients'>
-            Ingrédients
-          </Link>
+          <Link to='/ingredients'>Ingrédients</Link>
         </Menu.Item>
         <Menu.Item key='8' icon={<FormOutlined />}>
-          <Link to='/types-plats'>
-            Types de plats
-          </Link>
+          <Link to='/types-plats'>Types de plats</Link>
         </Menu.Item>
         <Menu.Item key='9' icon={<FormOutlined />}>
-          <Link to='/types-repas'>
-            Types de repas
-          </Link>
+          <Link to='/types-repas'>Types de repas</Link>
         </Menu.Item>
         <Menu.Item key='10' icon={<FormOutlined />}>
-          <Link to='/regimes'>
-            Tous les Régimes
-          </Link>
+          <Link to='/regimes'>Tous les Régimes</Link>
         </Menu.Item>
         <SubMenu key='sub4' icon={<CopyOutlined />} title='Pages'>
           <Menu.Item key='11'>
-            <Link to='/pages'>
-              Nos pages
-            </Link>
+            <Link to='/pages'>Nos pages</Link>
           </Menu.Item>
           <Menu.Item key='12'>
-            <Link to='/pages/ajouter'>
-              Ajouter
-            </Link>
+            <Link to='/pages/ajouter'>Ajouter</Link>
           </Menu.Item>
         </SubMenu>
         <Menu.Item key='13' icon={<TeamOutlined />}>
-          <Link to='/utilisateurs'>
-            Gérer les utilisateurs
-          </Link>
+          <Link to='/utilisateurs'>Gérer les utilisateurs</Link>
         </Menu.Item>
         <Menu.Item key='14' icon={<UserOutlined />}>
-          <Link to='/mon-profil'>
-            Mon Profil
-          </Link>
+          <Link to='/mon-profil'>Mon Profil</Link>
         </Menu.Item>
       </Menu>
     </Sider>
-
   );
 };
 
