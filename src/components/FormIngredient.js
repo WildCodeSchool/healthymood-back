6177d6d2227ab6152ed2a3760e3531a8f98c048b
Astarosa <baptiste.shortgibet@gmail.com>
2020-06-12 10:45:21 +0200

    Modal form war episode 2 : Goodbye modal

--- src/components/FormIngredient.js
+++ src/components/FormIngredient.js
@@ -3,14 +3,20 @@ import 'antd/dist/antd.css';
 import { Form, Input, Checkbox, Button } from 'antd';
 
 const FormIngredient = () => {
+
   const onFinish = values => {
     console.log('Received values of form:', values);
+  };
+
+  const onFinishFailed = errorInfo => {
+    console.log('Failed:', errorInfo);
   };
 
   return (
     <Form
       name='form'
       onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
       autoComplete='off'
     >
       <div>
@@ -20,8 +26,8 @@ const FormIngredient = () => {
         >
           <Input placeholder="Nom de l'ingrédient" required />
         </Form.Item>
-        <Form.Item name='is_allergen' valuePropName='checked'>
-          <Checkbox defaultChecked={false}>Allergène</Checkbox>
+        <Form.Item name='is_allergen' valuePropName='checked' noStyle>
+          <Checkbox>Allergène</Checkbox>
         </Form.Item>
         <Form.Item>
           <Button htmlType='submit'>Enregistrer</Button>
--- src/pages/Ingredients.js
+++ src/pages/Ingredients.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import ModalForm from '../components/ModalForm';
 import IngredientsList from '../components/IngredientsList';
 import FormIngredient from '../components/FormIngredient';
 
 const Ingredients = (props) => {
   return (
     <>
-      <ModalForm title='Ajouter un ingredient' component={FormIngredient} />
-      <hr />
+      <FormIngredient />
       <IngredientsList />
     </>
   );
