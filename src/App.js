f4f300f3095425988bd4de4dccd88e77a2d0c5c8
Astarosa <baptiste.shortgibet@gmail.com>
2020-06-29 15:57:53 +0200

    Feature/login (#33)
    * create login page and login form
    
    * add login form to connexion page
    
    * authentification in progress
    
    * authentification wip
    
    * some css
    
    * auth done redirection wip
    
    * wip
    
    * auth + privateroute + redirection done
    
    Co-authored-by: tmicka23 <mickathp42@gmail.com>
    Co-authored-by: Anaïs Huoch <anaisgagneur@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
+  Redirect
 } from 'react-router-dom';
+import AuthContext from './context/authContext';
+
 import { Layout } from 'antd';
 import './Styles/App.css';
 import SideBar from './components/SideBar';
 import TopBar from './components/TopBar';
 import Home from './pages/Home';
 import Articles from './pages/Articles';
-import AddArticle from './pages/AddArticle';
 import Recipes from './pages/Recipes';
-import AddRecipe from './pages/AddRecipe';
 import CategoryArticles from './pages/CategoryArticles';
 import CategoryRecipes from './pages/CategoryRecipes';
 import Ingredients from './pages/Ingredients';
@@ -20,44 +21,95 @@ import Dishes from './pages/Dishes';
 import Meals from './pages/Meals';
 import Diets from './pages/Diets';
 import Pages from './pages/Pages';
-import AddPage from './pages/AddPage';
 import Users from './pages/Users';
 import AdminProfil from './pages/AdminProfil';
+import AdminAuth from './pages/AdminAuth';
 
 const { Content } = Layout;
 
-function App () {
+const PrivateRoute = ({ children, ...rest }) => {
+  const { token } = useContext(AuthContext);
   return (
-    <div className='App'>
-      <Router>
-        <Layout style={{ minHeight: '100vh' }}>
-          <SideBar />
-          <Layout className='site-layout'>
-            <TopBar />
-            <Content style={{ margin: '0 16px' }}>
-              <Switch>
-                <Route exact path='/' component={Home} />
-                <Route exact path='/articles' component={Articles} />
-                <Route exact path='/articles/ajouter' component={AddArticle} />
-                <Route exact path='/recettes' component={Recipes} />
-                <Route exact path='/recettes/ajouter' component={AddRecipe} />
-                <Route exact path='/article_categories' component={CategoryArticles} />
-                <Route exact path='/categories-recettes' component={CategoryRecipes} />
-                <Route exact path='/ingredients' component={Ingredients} />
-                <Route exact path='/types-plats' component={Dishes} />
-                <Route exact path='/meal_types' component={Meals} />
-                <Route exact path='/regimes' component={Diets} />
-                <Route exact path='/pages' component={Pages} />
-                <Route exact path='/pages/ajouter' component={AddPage} />
-                <Route exact path='/utilisateurs' component={Users} />
-                <Route exact path='/mon-profil' component={AdminProfil} />
-              </Switch>
-            </Content>
-          </Layout>
-        </Layout>
-      </Router>
-    </div>
+    <Route
+      {...rest}
+      render={({ location }) =>
+        token ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/login',
+              state: { from: location }
+            }}
+          />
+      )}
+    />
   );
-}
+};
+
+const App = () => {
+  const [token, setToken] = useState(window.localStorage.getItem('authToken'));
+  const setTokenInLocalStorage = (token) => {
+    window.localStorage.setItem('authToken', token);
+    setToken(token);
+  };
+  return (
+    <AuthContext.Provider value={{ token, setToken: setTokenInLocalStorage }}>
+      <div className='App'>
+        <Router>
+          <Switch>
+            <Route exact path='/login'>
+              <AdminAuth />
+            </Route>
+            <Layout style={{ minHeight: '100vh' }}>
+              <SideBar />
+              <Layout className='site-layout'>
+                <TopBar />
+                <Content style={{ margin: '0 16px' }}>
+                  <PrivateRoute exact path='/'>
+                    <Home />
+                  </PrivateRoute>
+                  <PrivateRoute exact path='/articles'>
+                    <Articles />
+                  </PrivateRoute>
+                  <PrivateRoute exact path='/recettes'>
+                    <Recipes />
+                  </PrivateRoute>
+                  <PrivateRoute exact path='/categories-articles'>
+                    <CategoryArticles />
+                  </PrivateRoute>
+                  <PrivateRoute exact path='/categories-recettes'>
+                    <CategoryRecipes />
+                  </PrivateRoute>
+                  <PrivateRoute exact path='/ingredients'>
+                    <Ingredients />
+                  </PrivateRoute>
+                  <PrivateRoute exact path='/types-plats'>
+                    <Dishes />
+                  </PrivateRoute>
+                  <PrivateRoute exact path='/types-repas'>
+                    <Meals />
+                  </PrivateRoute>
+                  <PrivateRoute exact path='/regimes'>
+                    <Diets />
+                  </PrivateRoute>
+                  <PrivateRoute exact path='/pages'>
+                    <Pages />
+                  </PrivateRoute>
+                  <PrivateRoute exact path='/utilisateurs'>
+                    <Users />
+                  </PrivateRoute>
+                  <PrivateRoute exact path='/mon-profil'>
+                    <AdminProfil />
+                  </PrivateRoute>
+                </Content>
+              </Layout>
+            </Layout>
+          </Switch>
+        </Router>
+      </div>
+    </AuthContext.Provider>
+  );
+};
 
 export default App;
