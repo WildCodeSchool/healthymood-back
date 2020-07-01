2f260801db063944a903e3a4cce79b75ba65c3af
Astarosa <baptiste.shortgibet@gmail.com>
2020-07-01 13:15:53 +0200

    tinymce front wip

--- src/components/EditorComponent.js
+++ src/components/EditorComponent.js
@@ -3,37 +3,18 @@ import '../Styles/Editor.css';
 import { Editor } from '@tinymce/tinymce-react';
 import { Input, Button } from 'antd';
 
-const EditorComponent = () => {
+const EditorComponent = ({ pageTitle, onPageTitleChange, apiKey, initialValue, init, value, onEditorChange, onClick }) => {
   return (
     <div className='editor-container'>
-      <Input className='editor-title' size='large' placeholder='Ajouter un titre' />
+      <Input onChange={(e) => onPageTitleChange(e)} value={pageTitle} className='editor-title' size='large' placeholder='Ajouter un titre' />
       <Editor
-        apiKey={process.env.REACT_APP_API_KEY}
-        initialValue="<p>This is the initialorem Ipsum is simply
-                            dummy text of the printing and typesetting industry.
-                        Lorem Ipsum has been the industry's standard dummy text ever
-                        since the 1500s, when an unknown printer took a galley of type
-                        and scrambled it to make a type specimen book. It has survived
-                        not only five centuries, but also the leap into electronic
-                        typesetting, remaining essentially unchanged. It was popularised in
-                        the 1960s with the release of Letraset sheets containing Lorem
-                        Ipsum passages, and more recently
-                    with desktop publishing software like Aldus PageMaker
-                including versions of Lorem Ipsu content of the editor</p>"
-        init={{
-          height: 500,
-
-          autosave_interval: '2s',
-          plugins: [
-            'advlist autolink lists link image charmap print preview anchor',
-            'searchreplace visualblocks code fullscreen',
-            'insertdatetime media table paste code help wordcount'
-          ],
-          toolbar:
-            'undo redo | formatselect | bold italic backcolor blockquote | alignleft aligncenter alignright alignjustify | link image media | bullist numlist outdent indent | removeformat | help'
-        }}
+        apiKey={apiKey}
+        initialValue={initialValue}
+        init={init}
+        value={value}
+        onEditorChange={(e) => onEditorChange(e)}
       />
-      <Button className='editor-button' size='large'>Ajouter</Button>
+      <Button type='submit' onClick={onClick} className='editor-button' size='large'>Ajouter</Button>
     </div>
   );
 };
--- src/pages/Pages.js
+++ src/pages/Pages.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import EditorComponent from '../components/EditorComponent';
+import useEditor from '../hooks/useEditor';
 
 const Pages = () => {
+  const [pageTitle, content, handlePageTitle, handleEditorChange, handleSubmit] = useEditor('/generic_pages');
   return (
     <div>
       <h1>Pages</h1>
+      <EditorComponent
+        pageTitle={pageTitle}
+        onPageTitleChange={(e) => handlePageTitle(e)}
+        apiKey={process.env.REACT_APP_API_KEY}
+        initialValue=''
+        init={{
+          height: 500,
+          autosave_interval: '2s',
+          plugins: [
+            'advlist autolink lists link image charmap print preview anchor',
+            'searchreplace visualblocks code fullscreen',
+            'insertdatetime media table paste code help wordcount'
+          ],
+          toolbar:
+            'undo redo | formatselect | bold italic backcolor blockquote | alignleft aligncenter alignright alignjustify | link image media | bullist numlist outdent indent | removeformat | help'
+        }}
+        value={content}
+        onEditorChange={(e) => handleEditorChange(e)}
+        onClick={handleSubmit}
+      />
     </div>
   );
 };
