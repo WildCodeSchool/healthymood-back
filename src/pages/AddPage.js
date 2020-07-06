411f24bb9f1f8ba3a0a51e4b50175a314cd9b5df
Astarosa <baptiste.shortgibet@gmail.com>
2020-07-06 15:48:56 +0200

    add redirection for submit to pages + api get resquest for pages

--- src/pages/AddPage.js
+++ src/pages/AddPage.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Redirect } from 'react-router-dom';
 import EditorComponent from '../components/EditorComponent';
 import useEditor from '../hooks/useEditor';
 import API from '../services/API';
@@ -13,7 +14,8 @@ const AddPages = () => {
     loading: false,
     showToast: false,
     message: null,
-    success: false
+    success: false,
+    status: ''
   });
 
   const toastr = (success, show, message) => {
@@ -28,7 +30,7 @@ const AddPages = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setState({ ...state, loading: true });
+    setState({ ...state, loading: true, status: 'pending' });
     const data = e.target;
     formData = {
       title: data.title.value,
@@ -39,11 +41,11 @@ const AddPages = () => {
     };
     API.post('/generic_pages', formData)
       .then(res => {
-        setState({ ...state, showToast: true, success: true, message: `La page ${res.data.data.title} à été crée avec succès!` });
+        setState({ ...state, showToast: true, success: true, message: `La page ${res.data.data.title} à été crée avec succès!`, status: 'finished' });
       })
       .catch(err => {
         console.warn(err);
-        setState({ ...state, showToast: true, success: false, message: 'Erreur lors de la création de la page ' });
+        setState({ ...state, showToast: true, success: false, message: 'Erreur lors de la création de la page ', status: 'error' });
       })
       .finally(() => {
         setState({ ...state, loading: false });
@@ -64,40 +66,42 @@ const AddPages = () => {
         draggable
         progress={undefined}
       />
-      <h1>Pages</h1>
-      <form className='editor-form' onSubmit={(e) => handleSubmit(e)}>
-        <label className='hide-label' htmlFor='title'>titre</label>
-        <input type='text' name='title' placeholder='Ajouter un titre' required />
-        <label className='hide-label' htmlFor='slug'>slug</label>
-        <input type='text' name='slug' placeholder='Ajouter un slug' required />
-        <EditorComponent
-          apiKey={process.env.REACT_APP_API_KEY}
-          initialValue=''
-          init={{
-            height: 500,
-            autosave_interval: '5s',
-            plugins: [
-              'advlist autolink lists link image charmap print preview anchor',
-              'searchreplace visualblocks code fullscreen',
-              'insertdatetime media table paste code help wordcount',
-              'autosave'
-            ],
-            autosave_retention: '30m',
-            autosave_restore_when_empty: true,
-            toolbar:
-              'undo redo | formatselect | bold italic backcolor blockquote | alignleft aligncenter alignright alignjustify | link image media | bullist numlist outdent indent | removeformat | help'
-          }}
-          value={content}
-          onSaveContent={(e) => handleEditorChange(e)}
-        />
-        <label className='hide-label' htmlFor='content'>contenu</label>
-        <textarea className='hidden' name='content' value={content.content} />
-        <label htmlFor='published'>
-          Publier ?
-        </label>
-        <input type='checkbox' name='published' />
-        <button type='submit' className='btn' disabled={!!state.loading}>Ajouter</button>
-      </form>
+      {state.status === 'finished' ? <Redirect to={{ pathname: '/pages' }} />
+        : (
+          <form className='editor-form' onSubmit={(e) => handleSubmit(e)}>
+            <label className='hide-label' htmlFor='title'>titre</label>
+            <input type='text' name='title' placeholder='Ajouter un titre' required />
+            <label className='hide-label' htmlFor='slug'>slug</label>
+            <input type='text' name='slug' placeholder='Ajouter un slug' required />
+            <EditorComponent
+              apiKey={process.env.REACT_APP_API_KEY}
+              initialValue=''
+              init={{
+                height: 500,
+                autosave_interval: '5s',
+                plugins: [
+                  'advlist autolink lists link image charmap print preview anchor',
+                  'searchreplace visualblocks code fullscreen',
+                  'insertdatetime media table paste code help wordcount',
+                  'autosave'
+                ],
+                autosave_retention: '30m',
+                autosave_restore_when_empty: true,
+                toolbar:
+                'undo redo | formatselect | bold italic backcolor blockquote | alignleft aligncenter alignright alignjustify | link image media | bullist numlist outdent indent | removeformat | help'
+              }}
+              value={content}
+              onSaveContent={(e) => handleEditorChange(e)}
+            />
+            <label className='hide-label' htmlFor='content'>contenu</label>
+            <textarea className='hidden' name='content' value={content.content} />
+            <label htmlFor='published'>
+            Publier ?
+            </label>
+            <input type='checkbox' name='published' />
+            <button type='submit' className='btn' disabled={!!state.loading}>Ajouter</button>
+          </form>
+        )}
     </>
   );
 };
--- src/pages/Pages.js
+++ src/pages/Pages.js
@@ -1,10 +1,84 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../Styles/Editor.css';
+import API from '../services/API';
+import { ToastContainer, toast } from 'react-toastify';
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
+import 'react-toastify/dist/ReactToastify.css';
 
 const AddPage = () => {
+  const [pages, setPages] = useState([]);
+  const [state, setState] = useState({
+    loading: false,
+    showToast: false,
+    message: null,
+    success: false
+  });
+
+  const toastr = (success, show, message) => {
+    if (show) {
+      if (success) {
+        return toast.success(message);
+      } else {
+        return toast.error(message);
+      }
+    }
+  };
+
+  useEffect(() => {
+    API.get('/generic_pages')
+      .then(res => {
+        setPages(res.data.data);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+      .finally(() => {
+        setState({ ...state, loading: false });
+      });
+  }, []);
+
+  useEffect(() => {
+    toastr(state.success, state.showToast, state.message);
+  }, [state]);
+
+  console.log(pages);
+
   return (
     <>
-      <p>Listing des pages à venir</p>
+      <ToastContainer
+        position='top-right'
+        autoclose={5000}
+        closeOnClick
+        pauseOnHover
+        draggable
+        progress={undefined}
+      />
+      <table className='render-list'>
+        <thead>
+          <tr>
+            <td>Titre</td>
+            <td>Publié</td>
+            <td>Actions</td>
+          </tr>
+        </thead>
+        <tbody>
+          {pages.map(p => {
+            return (
+              <tr key={p.id}>
+                <td>{p.title}</td>
+                <td>
+                  {p.published === 0 ? 'Non' : 'Oui'}
+                </td>
+                <td>
+                  <EditOutlined className='edit-icon' />
+                  <DeleteOutlined className='delete-icon' />
+
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </>
   );
 };
