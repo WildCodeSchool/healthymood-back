c31f54d970946f0125e894c31006bffa0f43e292
Astarosa <baptiste.shortgibet@gmail.com>
2020-07-16 14:07:34 +0200

    Feature/upload image/recipes categories (#53)
    * pull + fix
    
    * add image upload
    
    * wip upload image

--- src/components/SingleSelect.js
+++ src/components/SingleSelect.js
@@ -5,18 +5,18 @@ import Select from 'react-select';
 const animatedComponents = makeAnimated();
 
 const NoOptionsMessage = props => {
-    return (
-        <div>{props.noMoreText ? props.noMoreText : "pas d'autre option"}</div>
-    );
+  return (
+    <div>{props.noMoreText ? props.noMoreText : "pas d'autre option"}</div>
+  );
 };
 
-export default function SingleSelect(props) {
-    return (
-        <Select
-            {...props}
-            closeMenuOnSelect={true}
-            components={{ ...animatedComponents, NoOptionsMessage }}
-            labelledBy='Select'
-        />
-    );
+export default function SingleSelect (props) {
+  return (
+    <Select
+      {...props}
+      closeMenuOnSelect
+      components={{ ...animatedComponents, NoOptionsMessage }}
+      labelledBy='Select'
+    />
+  );
 }
